65b4f14ddb8f7bf93299d256cb4f88623a6e8f68
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-08-31 00:59:50 +0500

    ui(summary): update

--- components/SummaryCell.jsx
+++ components/SummaryCell.jsx
@@ -1,8 +1,10 @@
 function SummaryCell() {
   return (
-    <div className="SummaryCell">
-      <div className="SummaryCell-box-wrapper"></div>
-    </div>
+    <>
+      <div className="SummaryCell">
+        <div className="SummaryCell-box-wrapper"></div>
+      </div>
+    </>
   );
 }
 

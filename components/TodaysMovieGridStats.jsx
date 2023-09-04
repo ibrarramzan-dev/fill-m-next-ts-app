505ffeec62e1ae4268fe08183a9bd45bfa4e3112
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-09-04 23:02:02 +0500

    feat-ui(game play): summary todays fill m stats done

--- components/TodaysMovieGridStats.jsx
+++ components/TodaysMovieGridStats.jsx
@@ -1,16 +1,30 @@
+import { useSelector } from "react-redux";
+
 function TodaysMovieGridStats() {
+  const scores = useSelector((state) => state.puzzle.stats.score);
+
+  let totalGames = 0;
+  let totalScoreByAllPlayers = 0;
+
+  Object.keys(scores).forEach((score) => {
+    totalScoreByAllPlayers += score * scores[score];
+    totalGames += scores[score];
+  });
+
+  let avgScore = totalScoreByAllPlayers / totalGames;
+
   return (
     <div className="TodaysMovieGridStats">
       <h3>Today’s Fill-M Stats</h3>
       <div className="TodaysMovieGridStats-wrapper">
         <div>
           <p>GAMES</p>
-          <p>73727</p>
+          <p>{totalGames}</p>
         </div>
 
         <div>
           <p>AVERAGE SCORE</p>
-          <p>4.0</p>
+          <p>{avgScore}</p>
         </div>
       </div>
     </div>

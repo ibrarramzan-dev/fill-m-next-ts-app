d9fe32d7a5c03101b2ada7552b183ae2220f8cb8
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-09-13 05:13:22 +0500

    fix-feat-ui(admin panel): mongodb atlas srv updated

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectMongoDB = () => {
   try {
+    // mongoose.connect(
+    //   "mongodb+srv://ibrarramzandev:yHdAwqL63mgtZJTO@cluster0.h8knxlu.mongodb.net/?retryWrites=true&w=majority"
+    // );
     mongoose.connect(
-      "mongodb+srv://ibrarramzandev:yHdAwqL63mgtZJTO@cluster0.h8knxlu.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://rhphillipsiii:QEkx3WQ3N6EryZW8@cluster0.xndlq0b.mongodb.net/?retryWrites=true&w=majority"
     );
   } catch (error) {
     console.error(error);

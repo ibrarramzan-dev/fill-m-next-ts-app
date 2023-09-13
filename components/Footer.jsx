2752698906ae64a4186b9e64628738ca20481fbc
Ibrar Ramzan <ibrarramzan.dev@gmail.com>
2023-09-14 03:45:55 +0500

    fix-ui(app): submit game feedback text added in footer and rules popup

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,23 +4,19 @@ function Footer() {
   return (
     <footer>
       <div className="Footer-disclaimer-wrapper">
-        <div>
-          <p>
-            Note: Fill-M does not own any of the film industry information
-            depicted within this site. All rights go to the rightful owners
-            (click images for attribution information).
-          </p>
-          <p>
-            Please submit any game feedback or ideas to{" "}
-            <Link href="mailto:fill.m.grid@gmail.com">
-              fill.m.grid@gmail.com
-            </Link>{" "}
-            or via twitter{" "}
-            <Link href="https://twitter.com/FillMGame" target="_blank">
-              @FillMGame
-            </Link>
-          </p>
-        </div>
+        <p>
+          Note: Fill-M does not own any of the film industry information
+          depicted within this site. All rights go to the rightful owners (click
+          images for attribution information).
+        </p>
+        <p>
+          Please submit any game feedback or ideas to{" "}
+          <Link href="mailto:fill.m.grid@gmail.com">fill.m.grid@gmail.com</Link>{" "}
+          or via twitter{" "}
+          <Link href="https://twitter.com/FillMGame" target="_blank">
+            @FillMGame
+          </Link>
+        </p>
       </div>
     </footer>
   );
